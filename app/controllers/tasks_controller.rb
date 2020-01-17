4e406596334fbe0339ade8786665e1b19990d1a3
ryuuto.aiki <ryuto.no1@ezweb.ne.jp>
2020-01-17 15:06:42 +0000

    heroku login2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,5 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:edit, :destroy]
 
   def index
       @tasks = Task.all.page(params[:page]).per(10)
